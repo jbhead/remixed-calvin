0d16439220fa7c1053ecb7656c018f10d6381922
Joshua Head <jbhead@gmail.com>
2019-10-07 19:50:12 +0000

    installed devise for creating a user accounts.

--- app/controllers/panels_controller.rb
+++ app/controllers/panels_controller.rb
@@ -1,2 +1,16 @@
 class PanelsController < ApplicationController
+  def new
+    @panels = Panel.new
+  end
+
+  def create
+    Panel.create(panel_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def panel_params
+    params.require(:panel).permit(:image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'comics#index'
   get 'about', to: 'comics#about'
+  resources :panels
 end
 
